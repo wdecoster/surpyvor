3702364a071657523ce825406c555344fa120f3b
wdecoster <decosterwouter@gmail.com>
2018-11-08 16:42:09 +0100

    pep8 changes, adding code to get SURVIVOR version

--- surpyvor/surpyvor.py
+++ surpyvor/surpyvor.py
@@ -2,6 +2,9 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from .version import __version__
 import subprocess
 import tempfile
+import sys
+import shlex
+import os
 
 
 def main():
@@ -11,7 +14,8 @@ def main():
                  args.estimate_distance, args.minlength, args.output)
 
 
-def sv_merge(samples, distance, callers, type_arg, strand_arg, estimate_distance_arg minlength, output):
+def sv_merge(samples, distance, callers, type_arg, strand_arg,
+             estimate_distance_arg, minlength, output):
     """
     Executes SURVIVOR merge, with parameters:
     -samples.fofn (samples, list)
@@ -27,26 +31,30 @@ def sv_merge(samples, distance, callers, type_arg, strand_arg, estimate_distance
         for s in samples:
             fofn.write(s + "\n")
     type, strand, estimate_distance = (0, 0, 0)
-    if type_arg: type = 1
-    if strand_arg: strand = 1
-    if estimate_distance_arg: estimate_distance = 1
+    if type_arg:
+        type = 1
+    if strand_arg:
+        strand = 1
+    if estimate_distance_arg:
+        estimate_distance = 1
     survivor_cmd = "SURVIVOR merge {} {} {} {} {} {} {} {}".format(
-                        fofn_f,
-                        distance,
-                        callers,
-                        type,
-                        strand,
-                        estimate_distance,
-                        minlength,
-                        output)
+        fofn_f,
+        distance,
+        callers,
+        type,
+        strand,
+        estimate_distance,
+        minlength,
+        output)
     sys.stderr.write("Executing SURVIVOR...\n")
     subprocess.call(shlex.split(survivor_cmd), stdout=subprocess.DEVNULL)
     os.close(fhf)
 
 
 def get_args():
+    survivor_version = get_survivor_version()
     parser = ArgumentParser(description="A wrapper around SURVIVOR",
-                            formatter_class=ArgumentDefaultsHelpFormatter))
+                            formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("-v", "--version",
                         action="version",
                         version='surpyvor: {}, SURVIVOR {}'.format(__version__, survivor_version),
@@ -56,14 +64,45 @@ def get_args():
                         required=True)
     subparsers = parser.add_subparsers(help='Available subcommands')
     merge = subparsers.add_parser("merge")
-    merge.add_argument("-f", "--files", nargs='+', required=True, help="vcf files to merge")
-    merge.add_argument("-d", "--distance", type=int, default=500, help="distance between variants to merge")
-    merge.add_argument("-l", "--minlength", type=int, default=50, help="Minimum length of variants to consider")
-    merge.add_argument("-c", "--callers", type=int, default=1, help="Minimum number of callers to support a variant")
-    merge.add_argument("-t", "--type", action="store_true", default=False, "Take type into account")
-    merge.add_argument("-s", "--strand", action="store_true", default=False, "Take strand into account")
-    merge.add_argument("-e", "--estimate_distance", action="store_true", default=False, "Estimate distance between calls")
+    merge.add_argument("-f", "--files",
+                       nargs='+',
+                       required=True,
+                       help="vcf files to merge")
+    merge.add_argument("-d", "--distance",
+                       type=int,
+                       default=500,
+                       help="distance between variants to merge")
+    merge.add_argument("-l", "--minlength",
+                       type=int,
+                       default=50,
+                       help="Minimum length of variants to consider")
+    merge.add_argument("-c", "--callers",
+                       type=int,
+                       default=1,
+                       help="Minimum number of callers to support a variant")
+    merge.add_argument("-t", "--type",
+                       action="store_true",
+                       default=False,
+                       help="Take type into account")
+    merge.add_argument("-s", "--strand",
+                       action="store_true",
+                       default=False,
+                       help="Take strand into account")
+    merge.add_argument("-e", "--estimate_distance",
+                       action="store_true",
+                       default=False,
+                       help="Estimate distance between calls")
     return parser.parse_args()
+
+
+def get_survivor_version():
+    for line in subprocess.check_output(args="SURVIVOR",
+                                        stderr=subprocess.STDOUT,
+                                        universal_newlines=True).split('\n'):
+        if line.startswith("Version:"):
+            return line.strip().split(' ')[1]
+    else:
+        return "version not found"
 
 
 if __name__ == '__main__':

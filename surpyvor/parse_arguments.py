9fd232332e24912adfa553373a589c7e7de5693b
wdecoster <decosterwouter@gmail.com>
2019-02-25 16:38:39 +0100

    making argument groups for required and optional arguments

--- surpyvor/parse_arguments.py
+++ surpyvor/parse_arguments.py
@@ -15,76 +15,82 @@ def get_args():
     subparsers = parser.add_subparsers(dest='command',
                                        title='[sub-commands]')
     merge = subparsers.add_parser("merge", help="merging vcf files of SVs")
-    merge.add_argument("-f", "--files",
-                       nargs='+',
-                       required=True,
-                       help="vcf files to merge")
-    merge.add_argument("-o", "--output",
-                       help="output file",
-                       required=True)
-    merge.add_argument("-d", "--distance",
-                       type=int,
-                       default=500,
-                       help="distance between variants to merge")
-    merge.add_argument("-l", "--minlength",
-                       type=int,
-                       default=50,
-                       help="Minimum length of variants to consider")
-    merge.add_argument("-c", "--callers",
-                       type=int,
-                       default=1,
-                       help="Minimum number of callers to support a variant")
-    merge.add_argument("-t", "--type",
-                       action="store_true",
-                       default=False,
-                       help="Take type into account")
-    merge.add_argument("-s", "--strand",
-                       action="store_true",
-                       default=False,
-                       help="Take strand into account")
-    merge.add_argument("-e", "--estimate_distance",
-                       action="store_true",
-                       default=False,
-                       help="Estimate distance between calls")
+    merge_req = merge.add_argument_group('required arguments')
+    merge_req.add_argument("-f", "--files",
+                           nargs='+',
+                           required=True,
+                           help="vcf files to merge")
+    merge_req.add_argument("-o", "--output",
+                           help="output file",
+                           required=True)
+    merge_opt = merge.add_argument_group('optional arguments')
+    merge_opt.add_argument("-d", "--distance",
+                           type=int,
+                           default=500,
+                           help="distance between variants to merge")
+    merge_opt.add_argument("-l", "--minlength",
+                           type=int,
+                           default=50,
+                           help="Minimum length of variants to consider")
+    merge_opt.add_argument("-c", "--callers",
+                           type=int,
+                           default=1,
+                           help="Minimum number of callers to support a variant")
+    merge_opt.add_argument("-t", "--type",
+                           action="store_true",
+                           default=False,
+                           help="Take type into account")
+    merge_opt.add_argument("-s", "--strand",
+                           action="store_true",
+                           default=False,
+                           help="Take strand into account")
+    merge_opt.add_argument("-e", "--estimate_distance",
+                           action="store_true",
+                           default=False,
+                           help="Estimate distance between calls")
     highsens = subparsers.add_parser("highsens", help="get union of SV vcfs")
-    highsens.add_argument("-f", "--files",
-                          nargs='+',
-                          required=True,
-                          help="vcf files to merge")
-    highsens.add_argument("-o", "--output",
-                          help="output file",
-                          required=True)
+    highsens_req = highsens.add_argument_group('required arguments')
+    highsens_req.add_argument("-f", "--files",
+                              nargs='+',
+                              required=True,
+                              help="vcf files to merge")
+    highsens_req.add_argument("-o", "--output",
+                              help="output file",
+                              required=True)
     highconf = subparsers.add_parser("highconf", help="get intersection of SV vcfs")
-    highconf.add_argument("-f", "--files",
-                          nargs='+',
-                          required=True,
-                          help="vcf files to merge")
-    highconf.add_argument("-o", "--output",
-                          help="output file",
-                          required=True)
+    highconf_req = highconf.add_argument_group('required arguments')
+    highconf_req.add_argument("-f", "--files",
+                              nargs='+',
+                              required=True,
+                              help="vcf files to merge")
+    highconf_req.add_argument("-o", "--output",
+                              help="output file",
+                              required=True)
     prf = subparsers.add_parser('prf', help="calculate precision, recall and F-measure")
-    prf.add_argument("--truth",
-                     help="vcf containing truth set",
-                     required=True)
-    prf.add_argument("--test",
-                     help="vcf containing test set",
-                     required=True)
-    prf.add_argument("-d", "--distance",
-                     help="maximum distance between test and truth call",
-                     default=500)
-    prf.add_argument("--minlength",
-                     help="Minimum length of SVs to be taken into account",
-                     default=50)
-    prf.add_argument("-i", "--ignore_type",
-                     help="Ignore the type of the structural variant",
-                     action="store_true")
-    prf.add_argument("--bar",
-                     help="Make stacked bar chart of SV lengths coloured by validation status",
-                     action="store_true")
-    prf.add_argument("--ignore_chroms",
-                     help="Chromosomes to ignore for prf calculation.",
-                     nargs='*',
-                     default=['chrEBV'])
+    prf_req = prf.add_argument_group('required arguments')
+    prf_req.add_argument("--truth",
+                         help="vcf containing truth set",
+                         required=True)
+    prf_req.add_argument("--test",
+                         help="vcf containing test set",
+                         required=True)
+    prf_opt = prf.add_argument_group('optional arguments')
+    prf_opt.add_argument("-d", "--distance",
+                         help="maximum distance between test and truth call",
+                         default=500)
+    prf_opt.add_argument("--minlength",
+                         help="Minimum length of SVs to be taken into account",
+                         default=50)
+    prf_opt.add_argument("-i", "--ignore_type",
+                         help="Ignore the type of the structural variant",
+                         action="store_true")
+    prf_opt.add_argument("--bar",
+                         help="Make stacked bar chart of SV lengths coloured by validation status",
+                         action="store_true")
+    prf_opt.add_argument("--ignore_chroms",
+                         help="Chromosomes to ignore for prf calculation.",
+                         nargs='*',
+                         default=['chrEBV'])
 
     args = parser.parse_args()
     if not args.command:

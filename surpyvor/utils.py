4c976ff06e5eac8630a9ea86d4a5ec8c8c4a4982
wdecoster <decosterwouter@gmail.com>
2019-02-26 13:38:23 +0100

    create a confusion matrix from a merged vcf

--- surpyvor/utils.py
+++ surpyvor/utils.py
@@ -3,6 +3,7 @@ import tempfile
 from cyvcf2 import VCF
 import subprocess
 import shlex
+import pandas as pd
 import gzip
 
 
@@ -49,3 +50,30 @@ def vcf_concat(vcffiles):
         inp=' '.join(vcffiles))
     subprocess.call(shlex.split(bcftools_concat_cmd))
     return concatenated
+
+
+def confusion_matrix(vcff, names):
+    """
+    First level of the dict is the "first" call, second level is the "second" sample
+    0: hom_ref
+    1: heterozygous
+    2: unknown/nocall
+    3: hom_alt
+    """
+    zygosities = {0: {0: 0, 1: 0, 2: 0, 3: 0},
+                  1: {0: 0, 1: 0, 2: 0, 3: 0},
+                  2: {0: 0, 1: 0, 2: 0, 3: 0},
+                  3: {0: 0, 1: 0, 2: 0, 3: 0},
+                  }
+    for v in VCF(vcff):
+        zygosities[v.gt_types[0]][v.gt_types[1]] += 1
+    zygs = [2, 0, 1, 3]
+    df = pd.DataFrame(index=zygs, columns=zygs)
+    for tr in zygs:
+        for te in zygs:
+            df.loc[tr, te] = zygosities[tr][te]
+    df.columns = ['nocall', 'hom_ref', 'het', 'hom_alt']
+    df.columns.name = names[1]
+    df.index = ['nocall', 'hom_ref', 'het', 'hom_alt']
+    df.index.name = names[0]
+    print(df)
